d8008f6b4675fe2b4944c271f2e41dfe38ade179
chandantm317 <chandantmchandu31@gmail.com>
2022-09-12 12:06:02 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+const Todo=()=>{
+
+  return(
+    
+    <>
+    <div classname="todo">
+
+      <h1> Todolist</h1>
+      <form>
+
+
+
+<label>
+  Add todo list
+</label>
+<input>add </input>
+
+
+      </form>
+
+    </div>
+
+
+    </>
+  )
+
+}
+export default Todo;
